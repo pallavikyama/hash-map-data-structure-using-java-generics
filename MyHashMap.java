ae65896bf73d9c6fb8983984faf07c15f2049e02
pallavikyama <kyamap98@gmail.com>
2020-10-12 23:28:10 +0530

    Added:New welcome message is added.

---
+++ MyHashMap.java
@@ -0,0 +1,7 @@
+package com.blz.hashmap;
+
+public class MyHashMap {
+	public static void main(String[] args) {
+		System.out.println("Welcome to creating a Hash_Map data structure using Java Generics.");
+	}
+}
